8e7c03f889df4573cc84b9af0edd1e0214356619
Mel Wright <codingwright@gmail.com>
2019-04-04 23:20:04 -0700

    Instagram team project basic set up.

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+// First route
+app.get('/', (req, res) => res.send('Hello'));
+
+const port = process.env.PORT || 5200;
+app.listen(port, () => console.log(`Server running on port ${port}`));
