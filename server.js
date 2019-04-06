3cc4ad3aba6785928beae10421da0e1d96db018d
anattaxo <liza.m.arnold@gmail.com>
2019-04-06 08:51:25 -0700

    post model, api and validation added

--- server.js
+++ server.js
@@ -6,6 +6,3 @@ app.get('/', (req, res) => res.send('Hello'));
 
 const port = process.env.PORT || 5200;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
-test123456767
---
+++ validation/post.js
@@ -0,0 +1,35 @@
+import validator from 'validate-image-url'
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+//Check if URL is valid image
+ 
+const promise = validator({url: data.image, timeout: 10000})
+  .then(({image, url}) => {
+    console.log('Image URL is valid.', 'URL:', url, 'image:', image)
+  })
+  .catch((err) => {
+    errors.image = 'Must be a valid image URL.';
+  })
+
+
+  // If image field is empty
+
+module.exports = function validatePostInput(data) {
+ let errors = {};
+
+  data.image = !isEmpty(data.image) ? data.image : '';
+
+  if (Validator.isEmpty(data.image)) {
+   errors.image = 'Image field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+  };
+
+
+
+  // found at: https://www.npmjs.com/package/validate-image-url
