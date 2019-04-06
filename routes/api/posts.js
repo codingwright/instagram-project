ac539f4ecc1ff703486e9af573e6dfe73720b9ea
Mel Wright <codingwright@gmail.com>
2019-04-06 10:59:53 -0700

    Said I needed to merge posts.js, though I didn't update this. I believe it's leftover from the merge conflict

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const passport = require('passport');
 
@@ -217,9 +216,5 @@ router.delete(
   }
 );
 
-=======
-
-router.get('/test', (req, res) => res.json({msg: 'Posts api works'}));
->>>>>>> 620ac38eba4663e0872a24b7e7e431025fa870e7
 
 module.exports = router;
