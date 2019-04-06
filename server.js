620ac38eba4663e0872a24b7e7e431025fa870e7
Nuri Kim <kimn8684@gmail.com>
2019-04-06 00:45:35 -0700

    adding bodyparser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const bodyparser = require('body-parser');
 
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
