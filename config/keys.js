f859dffb164fe81b06b57f99d27d87ab151d3d45
Mel Wright <codingwright@gmail.com>
2019-04-05 22:32:57 -0700

    MW user API in progress, also testing Slack github integration

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Team:learn2code@cluster0-8cjf4.azure.mongodb.net/test?retryWrites=true'
+}
+
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
+
+
+//Connect to db
+mongoose.connect(db).then(() => console.log('MongoDb connected')).catch(err => console.log(err));
 
 // First route
 app.get('/', (req, res) => res.send('Hello'));
 
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+
 const port = process.env.PORT || 5200;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
-test123456767
