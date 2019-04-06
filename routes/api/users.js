704c130cd81e8c588de58e273fee98c399c8f387
Mel Wright <codingwright@gmail.com>
2019-04-06 10:44:42 -0700

    MW User API and model cont

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
-router.get('/test', (req, res) => res.json({msg: 'Users api works'}));
+// Load user model
+const User = require('../../models/User');
+
+// @route POST api/users/register
+// @desc Register user
+// @access public
+router.post('/register', (req, res) => {
+    User.findOne({email: req.body.email}).then(user => {
+        if (user) {
+            return res.status(400).json({email: 'Email already exists'})
+        } else {
+            const avatar = gravatar.url(req.body.email, {
+                s:  '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                avatar
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                if (err) {
+                    return res.status(400).json({password: 'Failed encrypting'});
+                }
+
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) {
+                        return res.status(400).json({password: 'Failed hashing'});
+                    }
+    
+                    newUser.password = hash;
+
+                    newUser.save().then(user => res.json(user)).catch(err => console.log(err));
+                });
+            })
+
+        }
+    }).catch(err => console.log(err));
+});
+
+// @route POST api/users/login
+// @desc Login user
+// @access public
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email}).then(user => {
+        if (!user) {
+            return res.status(400).json({email: 'User not found'});
+        }
+
+        //Check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                return res.json({msg: 'Success'});
+            } 
+            return res.status(400).json({password: 'Password incorrect'});
+        });
+    }).catch(err => console.log(err));
+});
+
 
 module.exports = router;
