bedec061fc029c8c8218b7f8e3e00a256b0e1a16
Nuri Kim <kimn8684@gmail.com>
2019-04-30 13:53:01 -0700

    fixing error and post video

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ opts.secretOrKey = keys.secretOrKey;
 module.exports = passport => {
     passport.use(
         new JwtStrategy(opts, (jwt_payload, done) => {
-            User.findbyId(jwt_payload.id).then(user => {
+            User.findById(jwt_payload.id).then(user => {
                 if (user) {
                     return done(null, user);
                 }
